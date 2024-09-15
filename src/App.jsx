470dd6ba129f22a2796d0f8b249ee1ccb8dd31e7
Yann <ybarlet@gmail.com>
2024-09-15 18:11:34 +0200

    Implement combineReducers and filter notes feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import NewNote from "./NewNote";
 import Notes from "./Notes";
+import VisibilityFilter from "./VisibilityFilter";
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value);
+  };
+
   return (
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
 // import { StrictMode } from "react";
+import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import noteReducer from "./reducers/noteReducer";
+import filterReducer from "./reducers/filterReducer.js";
 import { Provider } from "react-redux";
 
-const store = createStore(noteReducer);
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
+
+const store = createStore(reducer);
+
+console.log(store.getState());
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
